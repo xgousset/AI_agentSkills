c25678329afe0bd38931b10ee469e0f30551b3cf
xgousset <145650582+xgousset@users.noreply.github.com>
2026-06-02 14:47:20 +0200

    base flask now working on port 5001

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Lancement de l'application Flask...
+.\.venv\Scripts\python.exe -m flask run --host=0.0.0.0 --port=5001
+pause
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Lancement de l'application Flask..."
+./.venv/bin/python -m flask run --host=0.0.0.0 --port=5001
